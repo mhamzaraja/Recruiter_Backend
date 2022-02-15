c23d982f3e78b5902e8c1ead82b0fc2da47d4345
hotaru29 <kz.cs.29@gmail.com>
2022-02-15 15:13:52 +0500

    removed validation, changed user controller, changed datatype candiadte profile experence

--- app/models/index.js
+++ app/models/index.js
@@ -36,6 +36,7 @@ db.candidateEducation = require("../models/candidate.education.model")(sequelize
 db.candidateProjects = require("../models/user.projects.model")(sequelize, Sequelize);
 db.candidateSkills = require("../models/user.skills.model")(sequelize, Sequelize);
 db.candidateLanguages = require("../models/user.languages.model")(sequelize, Sequelize);
+db.userExperience = require("../models/userExperience.model")(sequelize, Sequelize);
 
 //employer models
 db.employerProfile = require("../models/employer.profile.model")(sequelize, Sequelize);
@@ -43,9 +44,6 @@ db.jobPost = require("../models/job.post.model")(sequelize, Sequelize);
 db.jobSkills = require("../models/job.skills.model")(sequelize, Sequelize);
 db.jobLocation = require("../models/job.location.model")(sequelize, Sequelize);
 
-db.candidateProfile = require("../models/candidateProfile.model")(sequelize, Sequelize);
-db.candidateEducation = require("../models/education.model")(sequelize, Sequelize);
-db.userExperience = require("../models/userExperience.model")(sequelize, Sequelize);
 
 // ASSOCIATIONS
 
