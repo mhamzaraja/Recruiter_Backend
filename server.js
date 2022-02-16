d5d9993a52add58fa150abd902bf986b5f32b18e
Hamza Khan <mhamzak3@gmail.com>
2022-02-17 00:50:02 +0500

    Update Api

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ require("./app/routes/user.routes")(app);
 require("./app/routes/employer.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8002;
+const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
